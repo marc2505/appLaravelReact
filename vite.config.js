53108bffa1c4737da6720adfbf7960b6006806fc
marc2505 <marc.brunet@etu.hesge.ch>
2023-01-26 01:02:49 +0100

    Revert "FE_Comit_V1.0.0"
    This reverts commit ffbc498340cf9b469072d2f609f465437b84dad6.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.js'],
+            refresh: true,
+        }),
+    ],
+});
